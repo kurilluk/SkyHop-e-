30d1c37f21d5e612b09692ce5280e694e8c7cfb6
Lukas Kurilla <kurilluk@email.cz>
2025-03-30 14:41:19 +0200

    count in on the box - 3 seconds

--- sky/box.gd
+++ sky/box.gd
@@ -4,9 +4,11 @@ extends Area3D
 #@onready var _material = box_mesh.get_surface_override_material(0)
 
 var _type = "Jozko"
+var _active
 
 func _ready():
 	connect("body_entered", _on_body_entered)
+	_active = true
 
 	
 
@@ -30,9 +32,21 @@ func set_color(type: int):
 	else:
 		box_mesh.set_surface_override_material(0, new_material) # Aplikuje materiál na mesh
 
+func count_down():
+	pass
+
 func _on_body_entered(body):
 	#print(body)
+	if not _active:
+		return
+		
 	if body.is_in_group("player"):  # Uisti sa, že Player je v skupine "player"
-		#TODO ...body.is_on_ground
-		#print("Hráč vstúpil na: ", _type)
-		SignalManager.on_box_entered.emit(_type)  # Pošleme signál s menom boxu
+		var player = body as CharacterBody3D
+		if player and player.is_on_floor_only():
+			#print("on the floor")
+			#print("Hráč vstúpil na: ", _type)
+			SignalManager.on_box_entered.emit(_type)
+			_active = false  # Pošleme signál s menom boxu
+			await get_tree().create_timer(3).timeout
+			queue_free()
+			BgmManager.stop_box()
